86f2a2b25239ae59a03fd7519a221c9b108c43fb
harshendra-1998 <harshpraja66666@gmail.com>
2021-02-24 16:30:27 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -15,53 +15,61 @@ export default function App() {
   return (
     <div className="row">
       {data.map(datab => (
-        <div style={{padding:"10px"}} className="cardcell col-sm-12 col-md-6 col-lg-4 col-xl-3">
         <div
-          className="card"
-          style={{ flexMargin: "10px" }}
-          key={datab.id}
-          id={"cardd" + datab.id}
+          style={{ padding: "10px" }}
+          className="cardcell col-sm-12 col-md-6 col-lg-4 col-xl-3"
         >
-          <img
-            className="card-img"
-            src="https://www.w3schools.com/howto/img_avatar.png"
-          />
-          <section className="card-body">
-            <h3>{datab.name}</h3>
-            <p>
-              <i class="far fa-envelope" /> {datab.email}
-            </p>
-            <p>
-              <i class="fas fa-phone" /> {datab.phone}
-            </p>
-            <p>
-              <i class="fas fa-street-view" /> {datab.address.street},{" "}
-              {datab.address.suite}, {datab.address.city},{" "}
-              {datab.address.zipcode}{" "}
-            </p>
-            <p>
-              <i class="fas fa-globe"></i>  <a href={datab.website}>https://{datab.website}</a>
-            </p>
-            <p>Company : {datab.company.name}</p>
-          </section>
-          <ul className="nav nav-justified card-footer">
-            <li className="nav-item">
-              <a className="nav-link hearts" style={{ color: "red" }}>
-                <i class="far fa-heart" />
-              </a>
-            </li>
-            <li className="nav-item" data-toggle="modal" data-target="#myModal">
-              <a className="nav-link">
-                <i class="fas fa-edit" />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link closee">
-                <i class="fas fa-trash" />
-              </a>
-            </li>
-          </ul>
-        </div>
+          <div
+            className="card"
+            style={{ flexMargin: "10px" }}
+            key={datab.id}
+            id={"cardd" + datab.id}
+          >
+            <img
+              className="card-img"
+              src="https://www.w3schools.com/howto/img_avatar.png"
+            />
+            <section className="card-body">
+              <h3>{datab.name}</h3>
+              <p>
+                <i class="far fa-envelope" /> {datab.email}
+              </p>
+              <p>
+                <i class="fas fa-phone" /> {datab.phone}
+              </p>
+              <p>
+                <i class="fas fa-street-view" /> {datab.address.street},{" "}
+                {datab.address.suite}, {datab.address.city},{" "}
+                {datab.address.zipcode}{" "}
+              </p>
+              <p>
+                <i class="fas fa-globe" />{" "}
+                <a href={datab.website}>https://{datab.website}</a>
+              </p>
+              <p>Company : {datab.company.name}</p>
+            </section>
+            <ul className="nav nav-justified card-footer">
+              <li className="nav-item">
+                <a className="nav-link pushme" style={{ color: "red" }}>
+                  <i style={{}} class="far fa-heart haa" />
+                </a>
+              </li>
+              <li
+                className="nav-item"
+                data-toggle="modal"
+                data-target="#myModal"
+              >
+                <a className="nav-link">
+                  <i class="fas fa-edit" />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link closee">
+                  <i class="fas fa-trash" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       ))}
       <modal class="modal fade" id="myModal">
